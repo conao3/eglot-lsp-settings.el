2fbad092202184d2caa789b195e1958d5e4896dc
Naoya Yamashita <conao3@gmail.com>
2024-07-13 11:39:22 +0900

    generate project

---
+++ eglot-lsp-settings.el
@@ -0,0 +1,38 @@
+;;; eglot-lsp-settings.el --- Auto install Language-Server powered by vim-lsp-settings  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024  Naoya Yamashita
+
+;; Author: Naoya Yamashita <conao3@gmail.com>
+;; Version: 0.0.1
+;; Keywords: convenience
+;; Package-Requires: ((emacs "26.1"))
+;; URL: https://github.com/conao3/eglot-lsp-settings.el
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Auto install Language-Server powered by vim-lsp-settings.
+
+
+;;; Code:
+
+(defgroup eglot-lsp-settings nil
+  "Auto install Language-Server powered by vim-lsp-settings."
+  :group 'convenience
+  :link '(url-link :tag "Github" "https://github.com/conao3/eglot-lsp-settings.el"))
+
+(provide 'eglot-lsp-settings)
+
+;;; eglot-lsp-settings.el ends here
